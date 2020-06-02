565da7e30925e721483eb7a5de3ace1b68596d69
pjes0001 <pjes0001@UKLON-820XTN2.synchronoss.net>
2020-06-02 10:16:15 +0100

    New Maven Project

---
+++ src/test/java/userRegistration/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package userRegistration;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+}
